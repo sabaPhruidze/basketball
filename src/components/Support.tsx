1ccc02a35c565c1979a4836763a3ab5b9b9acd67
Saba Phruidze <sabaphruidze01@gmail.com>
2023-07-30 15:16:13 +0400

    I started making the helping component but It must be added many features . it takes time but I will do it during this days

--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import style from "./support.module.css";
+import BGFS from "../assets/img/BGFS.jpg";
 
 export default function Support() {
-  return <div>Support</div>;
+  const handleSubmit = (event:any) => {
+    event.preventDefault(); // prevent form submission
+    // You can add any custom logic here that you want to execute when the form is submitted
+  };
+
+  return (
+    <div>
+      <img src={BGFS} alt="BGIMG" className={style.img} />
+      <div className={style.BGColor} />
+      
+    </div>
+  );
 }
