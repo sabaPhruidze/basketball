e9d4b56517fb68915a63625496e2ffd18370d7da
Saba Phruidze <sabaphruidze01@gmail.com>
2023-08-01 14:40:18 +0400

    I will finish it quickly I think main part in support component is done

--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -1,76 +1,64 @@
-import react from "react";
-import {useContext,useEffect,useRef} from "react";
+import { useRef,useContext } from "react";
+import axios from "axios";
 import { myContext } from "./Root";
 import style from "./support.module.css";
 import EXIT from "../assets/icon/EXIT.svg"
 import BGFS from "../assets/img/BGFS.jpg";
-import axios from "axios";
-export default function Support() {
 
+
+export default function ContactForm() {
   const context = useContext(myContext);
-  const {dispatching,state} = context;
-  const {nameFS,emailFS,textFS,regexFS,sendFS} = state;
-  
-  const axiosInstance = axios.create({ 
-    baseURL: 'https://coding-world.com/back/public/api/fake-contact',
-    timeout: 1000,
-  });
+  const {dispatching,state,languageChanger} = context;
+  // const {nameFS,emailFS,textFS,regexFS,sendFS} = state;
 
+  const nameRef = useRef<any>("");
+  const emailRef = useRef<any>("");
+  const textRef = useRef<any>("");
 
-  
-    const sendContact =async () => {
-      const response = await axiosInstance.post("/add", { 
-        name: "saba",
-        email:"beeefa@gmail.com",
-        text:"1423324",
-      })
-     }
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://coding-world.com/back/public/api/fake-contact/add",
+        {
+          name: nameRef.current.value,
+          email: emailRef.current.value,
+          text: textRef.current.value,
+        }
+      );
+      nameRef.current.value = "";
+      emailRef.current.value = "";
+      textRef.current.value = "";
 
- 
-    const SEND_OR_NOT_DATA = [{data1:"Congratulations your message sent succesfully",data2:"!!! remember you must write at least 3 letters in name input, 12 letters in email input and seven letters in message"}]
+      console.log("Response:", response.data);
+      // Do something with the response if needed
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle the error if needed
+    }
+  };
+
   return (
     <div>
-      <img src={BGFS} alt="BGIMG" className={style.img} />
-      <div className={style.BGColor} />
-      {SEND_OR_NOT_DATA.map((data,idx) => (
-         regexFS === 1 ? <div className={style.sendedOrNot} key={idx}> <img src={EXIT} alt="EXIT" className={style.EXIT}  onClick={() => dispatching("regexForSupport",null)}/> {data.data1}</div> : regexFS === 2 ? <div className={style.sendedOrNot} key={idx + 1}> <img src={EXIT} alt="EXIT" className={style.EXIT} onClick={() => dispatching("regexForSupport",null)}/>{data.data2}</div> : ''
-      ))}
-      <div className={style.form}>
-        <input type="text" placeholder="სახელი"/>
-        <input type="email" placeholder="ემაილი"/>
-        <textarea></textarea>
-        <button onClick={() => sendContact()}>გაგზავნა</button>
+    <img src={BGFS} alt="BGIMG" className={style.img} />
+    <div className={style.BGColor} />
+      <div className={style.dFlex}>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>{languageChanger('სახელი','名前','Name')}</label>
+            <input type="text" ref={nameRef} pattern=".{3,}" placeholder={languageChanger('გთხოვთ მიუთითოთ თქვენი სახელი',"あなたの名前を入力してください",'Please enter your name')}/>
+          </div>
+          <div>
+            <label>{languageChanger('ელ-ფოსტა','メール','Email')}</label>
+            <input type="email" ref={emailRef} pattern="^[a-zA-Z0-9._%+-]+@gmail\.com$" placeholder={languageChanger('გთხოვთ მიუთითოთ თქვენი ელ-ფოსტა',"メールアドレスを入力してください",'Please enter your e-mail address')}/>
+          </div>
+          <div>
+            <label>{languageChanger('შეტყობინება',"メッセージ","Message")}</label>
+            <textarea ref={textRef} placeholder={languageChanger('გთხოვთ ჩაწეროთ თქვენი შეკითხვა',"質問を入力してください",'Please enter your question')}/>
+          </div>
+          <button type="submit">{languageChanger('გაგზავნა',"送信する","Submit")}</button>
+        </form>
       </div>
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
- 
-    // sendRef = useRef(
-    //   {
-    //     name: nameFS,
-    //     email: emailFS,
-    //     text: textFS,
-    //   }
-    // );
-  
-    // useEffect(() => {
-    //   const createUser = async () => {
-    //     const response = await axios.post(
-    //       "https://coding-world.com/back/public/api/fake-contact/add",
-          
-    //     );
-    //   };
-    //   console.log("sf");
-    //   createUser(); // the function must be summond after writing eat
-    // }, []);
-  
