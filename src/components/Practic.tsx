ad74ece137986cf2542f8f4c50a6fbf50f91164c
Saba Phruidze <sabaphruidze01@gmail.com>
2023-08-02 13:47:07 +0400

    So currently I made a filtering function so It filters with writing the name

--- src/components/Practic.tsx
+++ src/components/Practic.tsx
@@ -1,19 +1,66 @@
+import React, { useState } from 'react';
 import Select from 'react-select';
 import style from "./practic.module.css";
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+interface IframeData {
+  name: string;
+  src: string;
+}
+
+const iframeData: IframeData[] = [
+  {
+    name: "Beginning",
+    src: "https://www.youtube.com/embed/GYGzu9afduQ?list=PLWgKm7kgxzCuaTMjQ0mYmOgsh62LPcq5O",
+  },
+  {
+    name: "first move for begginers",
+    src: "https://www.youtube.com/embed/XbtmGKif7Ck?list=PLWgKm7kgxzCuaTMjQ0mYmOgsh62LPcq5O",
+  },
+];
+
 export default function Practic() {
-  const name = ["Apple", "sour", "factual"]
-let searchName = name?.map((item) => ({value:item, label:item}));
+  const [selectedOption, setSelectedOption] = useState<Option | null>(null);
+
+  const nameOptions = iframeData.map((data) => ({
+    value: data.name,
+    label: data.name,
+  }));
+
+  const filteredIframeData = selectedOption
+    ? iframeData.filter((data) =>
+        data.name.toLowerCase().includes(selectedOption.value.toLowerCase())
+      )
+    : iframeData;
+
   return (
     <div>
       <div className={style.backgroundP}></div>
       <Select
-        options={searchName} 
-        closeMenuOnSelect={false}         
+        options={nameOptions}
+        value={selectedOption}
+        onChange={(option) => setSelectedOption(option)}
+        closeMenuOnSelect={false}
         className={style.selectP}
- />
-    <div className={style.videoContainerP}>
-      {/* <iframe width="250px" height="180px" src="https://www.youtube.com/embed/VIDEO_ID" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe> */}
-    </div>
+      />
+      <div className={style.videoContainerP}>
+        {filteredIframeData.map((data) => (
+          <div className={style.dFlexColumn}>
+            <iframe
+              width="250"
+              height="180"
+              src={data.src}
+              title={data.name}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
+            <p className={style.videoName}>{data.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
