146fe1b232b289eebdc542a5a4ce7ad501bc51a9
Saba Phruidze <sabaphruidze01@gmail.com>
2023-08-02 14:04:36 +0400

    I will make this page scroll

--- src/components/Practic.tsx
+++ src/components/Practic.tsx
@@ -12,7 +12,7 @@ interface IframeData {
   src: string;
 }
 
-const iframeData: IframeData[] = [
+const iframeData1: IframeData[] = [
   {
     name: "Beginning",
     src: "https://www.youtube.com/embed/GYGzu9afduQ?list=PLWgKm7kgxzCuaTMjQ0mYmOgsh62LPcq5O",
@@ -22,21 +22,35 @@ const iframeData: IframeData[] = [
     src: "https://www.youtube.com/embed/XbtmGKif7Ck?list=PLWgKm7kgxzCuaTMjQ0mYmOgsh62LPcq5O",
   },
 ];
+const iframeData2: IframeData[] = [
+  {
+    name: "intermediate Beginning",
+    src: "https://www.youtube.com/embed/oADaM2L1YLc",
+  },
+  {
+    name: "first move for Intermediate",
+    src: "https://www.youtube.com/embed/HAD-dv4-r1U",
+  },
+];
 
 export default function Practic() {
   const [selectedOption, setSelectedOption] = useState<Option | null>(null);
-
-  const nameOptions = iframeData.map((data) => ({
+  const forNameOptions = [...iframeData1,...iframeData2]
+  const nameOptions = forNameOptions.map((data) => ({
     value: data.name,
     label: data.name,
   }));
 
-  const filteredIframeData = selectedOption
-    ? iframeData.filter((data) =>
+  const filteredIframeData1 = selectedOption
+    ? iframeData1.filter((data) =>
         data.name.toLowerCase().includes(selectedOption.value.toLowerCase())
       )
-    : iframeData;
-
+    : iframeData1;
+    const filteredIframeData2 = selectedOption
+    ? iframeData2.filter((data) =>
+        data.name.toLowerCase().includes(selectedOption.value.toLowerCase())
+      )
+    : iframeData2;
   return (
     <div>
       <div className={style.backgroundP}></div>
@@ -48,10 +62,24 @@ export default function Practic() {
         className={style.selectP}
       />
       <div className={style.videoContainerP}>
-        {filteredIframeData.map((data) => (
+        {filteredIframeData1.map((data) => (
           <div className={style.dFlexColumn}>
             <iframe
-              width="250"
+              width="200"
+              height="180"
+              src={data.src}
+              title={data.name}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
+            <p className={style.videoName}>{data.name}</p>
+          </div>
+        ))}
+      </div>
+      <div className={style.videoContainerP}>
+        {filteredIframeData2.map((data) => (
+          <div className={style.dFlexColumn}>
+            <iframe
+              width="200"
               height="180"
               src={data.src}
               title={data.name}
